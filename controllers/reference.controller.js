141e121472baf4af758c4a7798ea0b376c1cbfa3
DavidPinho <davidpinho08@gmail.com>
2018-01-13 22:41:51 +0000

    Retrieve all commits in reference route

--- controllers/reference.controller.js
+++ controllers/reference.controller.js
@@ -5,7 +5,6 @@ import ReferenceModel from './../models/reference.model';
 exports.findByRepository = (req, res, next) => {
     ReferenceModel.find(
         { repository: mongoose.Types.ObjectId(req.params.repository_id) },
-        { commits: { $slice: 1 } }
     )
     .then(r => res.json(r))
     .catch(e => next(e));
