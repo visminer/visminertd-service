28946026adaa8f2cc650ab1e7e35ca78c8bb52fb
Felipe Gustavo de Souza Gomes <felipegustavo1000@gmail.com>
2018-01-28 17:30:11 -0300

    Added a new method to the findbugs service.

--- controllers/findbugs.controller.js
+++ controllers/findbugs.controller.js
@@ -1,1 +1,13 @@
 import FindBugsModel from './../models/findbugs.model';
+
+exports.getByCommitAndFilehash = (req, res, next) => {
+    FindBugsModel.findOne({
+        commit: req.params.commit,
+        filehash: req.params.filehash
+    }, {
+        bugs: 1
+    })
+    .lean()
+    .then(r => res.json(r))
+    .catch(e => next(e));
+}
--- routes/api/findbugs.route.js
+++ routes/api/findbugs.route.js
@@ -8,4 +8,7 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: FindBugsController.helloWord() });
 });
 
+// GET
+router.get('/get/commit/:commit/filehash/:filehash', FindBugsController.getByCommitAndFilehash);
+
 export default router;
